1dc2e0c95982e188b080f1d1fa244e9b6e74e967
Kaio Batista <kaiobatist@gmail.com>
2022-03-25 09:02:11 -0300

    :recycle:Refactor some code parts

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-alfabeto = 'abcdefghijklmnopqrstuvwxyz'
-
-arquivo = './palavras_disponiveis.txt'
-
-
-def contar_letras(lista):
+def contar_letras(lista: list):
     letras_c = dict()
     for l in alfabeto:
         letras_c[l] = 0
@@ -22,10 +17,12 @@ def organizar_dict(dicio):
 
     return sorted_dicio
 
-letras_count = dict()
-for l in alfabeto:
-    letras_count[l] = 0
-total_de_palavras = 6026
+
+alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+
+arquivo = './palavras_disponiveis.txt'
+
+letras_count = {l: 0 for l in alfabeto}
 principais_palavras = list()
 
 palavra_alvo = {0:'', 1:'', 2:'', 3:'', 4:''}
@@ -33,6 +30,7 @@ lugares_errados = {0: '', 1: '', 2: '', 3: '', 4: ''}
 letras_certas = list()
 letras_erradas = list()
 
+# Conta quantas letras diferentes
 with open(arquivo, 'r') as f:
     for word in f.readlines():
         word = word.replace('\n', '')
@@ -40,11 +38,9 @@ with open(arquivo, 'r') as f:
             if l in word:
                 letras_count[l] += 1
 
-value_key_pairs = [(k, v) for (v, k) in letras_count.items()]
-sorted_value_key_pairs = sorted(value_key_pairs, reverse=True)
+letras_count = organizar_dict(letras_count)
 
-letras_count = {k: v for v, k in sorted_value_key_pairs}
-
+# Aponta as principais palavras para começar
 with open(arquivo, 'r') as f:
     for word in f.readlines():
         counter = 0
@@ -54,15 +50,11 @@ with open(arquivo, 'r') as f:
                 counter += 1
         if counter == 5:
             principais_palavras.append(word)
-        if len(principais_palavras) >= 20:
+        if len(principais_palavras) >= 5:
             break
 
 principais_palavras = list(set(principais_palavras[:]))
-print(principais_palavras)
-
 print(f'Palavras iniciais recomendadas: {", ".join(principais_palavras)}')
-
-count = 0
 
 while True:
     print('Qual palavra você usou?')
@@ -73,10 +65,13 @@ while True:
         print(f'Digite a {i + 1}ª letra descoberta e a posição [pos 0 = nao sabe a posicao]: ')
         lt, pos = input('>> ').split(' ')
         letras_certas.append(lt)
+                
         if int(pos) == 0:
             for i, j in enumerate(palavra_usada):
                 if j == lt: lugares_errados[i] += lt
         if int(pos) > 0: palavra_alvo[int(pos) - 1] = lt
+
+    # Define as letras incorretas
     for l in palavra_usada:
         if l not in letras_certas:
             letras_erradas.append(l)
@@ -87,22 +82,25 @@ while True:
         for word in f.readlines():
             word = word.replace('\n', '')
             tem_letra_errada = None
+            
             for l in letras_erradas:
-                if l in word:
-                    tem_letra_errada = True
+                if l in word: tem_letra_errada = True
+                
             if not tem_letra_errada:
+
                 qnt_certas_na_palavra = 0
                 for l  in letras_certas:
-                    if l in word: 
-                        qnt_certas_na_palavra += 1
+                    if l in word: qnt_certas_na_palavra += 1
                 if qnt_certas_na_palavra == len(letras_certas): # Verifica se a palavra tem as letras marcadas como corretas
-                    test = False
+                    test = None
                     for k, v in lugares_errados.items():
                         if word[k] in v:
                             test = True
                     if not test:
+
                         if ''.join(list(palavra_alvo.values())) in '        ':
                                 palavras_possiveis.append(word)
+
                         else:
                             qnt_certas_na_palavra = 0
                             for k, v in palavra_alvo.items():    
@@ -114,9 +112,11 @@ while True:
     
     palavras_possiveis = list(set(palavras_possiveis))
     letras_count = contar_letras(palavras_possiveis)
+
     for l in palavra_usada:
         if l in list(letras_count.keys()):
             letras_count.pop(l)
+                        
     letras_count = organizar_dict(letras_count)
     sis_pontuacao = {k: list(letras_count.keys()).index(k) + 1 for k in letras_count.keys()}
     palavra_pontuacao = list()
